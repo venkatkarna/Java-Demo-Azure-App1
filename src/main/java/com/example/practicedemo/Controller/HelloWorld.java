92126bb0fad8de522c2c773116c332e8cbae9354
venkatesh.b <venkatesh.b@changepond.com>
2024-01-30 14:42:51 +0530

    firstcommit

---
+++ src/main/java/com/example/practicedemo/Controller/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.practicedemo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld{
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Congrats! you are successfully deployed your application in Azure app service";
+    }
+    
+}
---
+++ src/test/java/com/example/practicedemo/PracticeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
